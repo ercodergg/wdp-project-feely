766f69a5bb47600604bbe2762d9bdba29ce06d3b
ercodergg <egg125@alu.ua.es>
2025-11-08 15:08:49 +0100

    third commit

--- feely/src/views/home.js
+++ feely/src/views/home.js
@@ -1,16 +1,40 @@
 import levelIcon from '../pngicons/level.png'
 import planIcon from '../pngicons/plan.png'
+
+const quotes = [
+  "Breathe, feel, be honest with yourself.",
+  "Your emotions are valid, even when messy.",
+  "Pause. Listen. Let it move through you.",
+  "You are allowed to feel everything.",
+  "Naming your feeling is the first step to healing.",
+  "Softness is strength. Vulnerability is clarity."
+]
+
 export function renderHome(container) {
+  const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
 
   container.innerHTML = `
-
+    <h1>Welcome to Feely!!</h1>
+    <div class="quote-container">
+      <h3 id="quote-text">“${randomQuote}”</h3>
+    </div>
     <section class="buttons-home">
       <a data-view="level-now">
-        <img class="icon-home" id="level" src="${levelIcon}"/>
+        <img class="icon-home" id="level" src="${levelIcon}" />
       </a>
       <a data-view="week-plan">
-        <img class="icon-home" id="plan" src="${planIcon}"/>
+        <img class="icon-home" id="plan" src="${planIcon}" />
       </a>
     </section>
   `
+
+  // Transición suave al cargar la quote
+  const quoteEl = container.querySelector('#quote-text')
+  quoteEl.style.opacity = '0'
+  quoteEl.style.transform = 'translateY(10px)'
+  setTimeout(() => {
+    quoteEl.style.transition = 'opacity 1s ease, transform 1s ease'
+    quoteEl.style.opacity = '1'
+    quoteEl.style.transform = 'translateY(0)'
+  }, 100)
 }
