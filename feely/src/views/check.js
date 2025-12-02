9af82e91be9ae9bd921fc9d2e42b4d04ff2c473f
ercodergg <egg125@alu.ua.es>
2025-12-02 15:16:13 +0100

    Better design different backgrounds

--- feely/src/views/check.js
+++ feely/src/views/check.js
@@ -1,39 +1,68 @@
+import levelIcon from '../pngicons/level.png';
 export function renderCheck(container) {
   container.innerHTML = `
     <button class="back-button"  data-view="home"> < Back</button>
     <h1 class="welcome">Check</h1> 
-    <div class="quote-container">
-<p class="quote-text" id="name-message"></p>
-</div>
-            <div class="left-column">
     
-  </div>
-
+<h3 class="quote-text text-block" id="name-message"></h3>
     <ul class="question-list">
       <li>
-      <h2>80%</h2>
+        <div class="quote-container">
+            <p class="quote-text"> Energy: Can yoy workout or be focused?</p>
+        </div>
         <div class="question-container">
           <button class="button-home" data-view="check">
             <img data-view="check" class="icon-home" " />
-            <b data-view="check">Energy</b>
+            <b data-view="check">Tired</b>
+          </button>
+          <button class="button-home" data-view="check">
+            <img data-view="check" class="icon-home" " />
+            <b data-view="check">Enough</b>
+          </button>
+          <button class="button-home" data-view="check">
+            <img data-view="check" class="icon-home" " />
+            <b data-view="check">Energetic</b>
           </button>
         </div>
       </li>
-            <li>
-            <h2>80%</h2>
+      <li >
+        <div class="quote-container">
+            <p class="quote-text">Expectations: How do you feel about
+            the future?</p>
+        </div>
         <div class="question-container">
+          <button class="button-home" data-view="check">
+            <img data-view="check" class="icon-home" />
+            <b data-view="check">Pesimist</b>
+          </button>
+          <button class="button-home" data-view="check">
+            <img data-view="check" class="icon-home" " />
+            <b data-view="check">Nihilist</b>
+          </button>
           <button class="button-home" data-view="check">
             <img data-view="check" class="icon-home" " />
             <b data-view="check">Optimism</b>
           </button>
         </div>
       </li>
+      
       <li>
-      <h2>80%</h2>
+        <div class="quote-container">
+            <p class="quote-text">Fulfillment: How do you feel about, the 
+            things you are currently doing?</p>
+        </div>
         <div class="question-container">
-          <button class="button-home" data-view="goals">
-            <img data-view="goals" class="icon-home" />
-            <b data-view="goals">Fulfillment</b>
+          <button class="button-home" data-view="check">
+            <img data-view="check" class="icon-home" " />
+            <b data-view="check">Sad</b>
+          </button>
+          <button class="button-home" data-view="check">
+            <img data-view="check" class="icon-home" " />
+            <b data-view="check">Neutral</b>
+          </button>
+          <button class="button-home" data-view="check">
+            <img data-view="check" class="icon-home" " />
+            <b data-view="check">Happy</b>
           </button>
         </div>
       </li>
--- feely/src/views/home.js
+++ feely/src/views/home.js
@@ -1,7 +1,8 @@
 import levelIcon from '../pngicons/level.png';
 import planIcon from '../pngicons/plan.png';
 import tickIcon from '../pngicons/tick.png';
-
+import logoutIcon from '../pngicons/logout.png';
+import { navigate } from '../main.js';
 const quotes = [
   "Breathe, feel, be honest with yourself.",
   "Your emotions are valid, even when messy.",
@@ -17,6 +18,10 @@ export function renderHome(container) {
   let user = JSON.parse(storedUser);
   const username = user ? user.username : 'User';
   container.innerHTML = `
+    <button class="back-button" id="logout"> 
+    <img data-view="check" class="icon-home" src="${logoutIcon}"/>
+    <b>Logout</b>
+    </button>
     <h1 class="welcome">Welcome to Feely!!</h1>
 
 <section class="home-section">
@@ -53,41 +58,77 @@ export function renderHome(container) {
   <!-- Columna derecha: reflexión -->
   <div class="right-column">
     <section class="reflexion-container">
-      <div class="quote-container">
-        <p class="quote-text">“${randomQuote}”</p>
-      </div>
-      <button class="button-home" id="write">
-        <img class="icon-home" src="./src/pngicons/light.png" alt="Write" />
-        <p>Write your own quote ideas</p>
-      </button>
-      <div class="background"></div>
-      <p>Here you can write down <br> the quotes you want to see everyday</p>
+      <div class="background" data-bg="./src/backgrounds/relax1-background.jpg"></div>
     </section>
   </div>
 </section>
 <section class="home-section">
   <div class="left-column">
-    <div class="text-block">
-      <p>Here you can write down the quotes you want to see everyday</p>
-      <p>Here you can write down the quotes you want to see everyday</p>
-      <p>Here you can write down the quotes you want to see everyday</p>
-    </div>
+      <div class="reflexion-container">
+      <div class="background" data-bg="./src/backgrounds/reflexion-background.jpg"></div>
+      <div class="quote-container">
+      <p class="quote-text">Write down the new goals you have and then check them when completed.</p>
+      </div>
+      <button class="button-home" id="write">
+<img class="icon-home" src="./src/pngicons/light.png" alt="Write" />
+<b>Write your own quote ideas</b>
+</button>
+      <p>Here you can write down</p>
+      <p>the quotes you want see everyday</p>
+  </div>
+  <div class="right-column">
+      <section class="reflexion-container">
+      <p>Write down what you would like to read</p>   
+      <p> when you wake up, </p>
+      <p>when you are lost, </p>
+      <p>what connects you with yourself.</p>
+       </div>
+    </section>
+  </div>
+</section>
+<section class="home-section">
+  <div class="left-column">
+  <section class="reflexion-container">
+  <button class="button-home" id="write">
+        <img data-view="goals" class="icon-home" src="${tickIcon}" />
+    <b>Create your goals</b>
+  </button>
+      <div class="quote-container">
+        <p class="quote-text">Write down the new goals you have and then check them when completed.</p>
+      </div>
+      <div class="background" data-bg="./src/backgrounds/note-background.jpg"></div>
+    </section>
   </div>
   <div class="right-column">
     <div class="reflexion-container">
-      <p>Here you can write down the quotes you want to see everyday</p>
-      <p>Here you can write down the quotes you want to see everyday</p>
-      <p>Here you can write down the quotes you want to see everyday</p>
+        <p>Ideally, you should start with</p>
+      <p>short-term goals.</p>
+      <p>This gives you momentum,</p>
+      <p>clarity, and confidence.</p> 
     </div>
   </div>
 </section>
 
 
   `
+  document.querySelectorAll('.background').forEach(el => {
+    const bg = el.getAttribute('data-bg');
+    if (bg) {
+      el.style.backgroundImage = `url(${bg})`;
+    }
+  });
 
   const h1_move = container.querySelector('.welcome');
   h1_move.style.transform = 'translateY(60px)';
   h1_move.style.opacity = '0';
+
+  let blogout = container.querySelector('#logout');
+  blogout.addEventListener('click', (event) => {
+    event.preventDefault();
+    localStorage.clear();
+
+    navigate('login');
+  });
 
   setTimeout(() => {
 
